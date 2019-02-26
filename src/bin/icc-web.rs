4353b8effd1afe8f8d18de44607d09955e8559a5
Emil H. Clausen (SEQUOIIA) <sequoiia@hummel.yt>
2019-02-26 22:10:29 +0100

    Begun work on integrated web interface

---
+++ src/bin/icc-web.rs
@@ -0,0 +1,128 @@
+extern crate pretty_env_logger;
+extern crate log;
+extern crate actix_web;
+
+use actix_web::http::{header, Method, StatusCode, HttpTryFrom};
+use actix_web::middleware::{Middleware, Finished, Response, Started};
+use actix_web::middleware::session::{self, RequestSession};
+use actix_web::{error, fs, middleware, pred, server, App, Error, HttpRequest, HttpResponse, Path, Result, ws, actix::Actor, actix::StreamHandler};
+use log::{error, info, debug};
+use std::env;
+use std::sync::{Arc, RwLock};
+
+struct GlobalData {
+    pub is_down : bool
+}
+
+impl GlobalData {
+    pub fn new() -> Arc<RwLock<GlobalData>> {
+        Arc::new(RwLock::new(GlobalData { is_down: false }))
+    }
+}
+
+// Default headers middleware
+struct SetDefaultHeaders;
+
+impl<S> Middleware<S> for SetDefaultHeaders {
+    fn start(&self, req: &HttpRequest<S>) -> Result<Started> {
+        Ok(Started::Done)
+    }
+
+    fn response(&self, _req: &HttpRequest<S>, mut resp: HttpResponse) -> Result<Response> {
+        resp.headers_mut().insert(
+            header::HeaderName::try_from("server").unwrap(),
+            header::HeaderValue::from_static("icc"));
+        Ok(Response::Done(resp))
+    }
+
+    fn finish(&self, _req: &HttpRequest<S>, _resp: &HttpResponse) -> Finished {
+        Finished::Done
+    }
+}
+
+// Websocket stream
+struct Ws;
+
+impl Actor for Ws {
+    type Context = ws::WebsocketContext<Self, Arc<RwLock<GlobalData>>>;
+}
+
+impl StreamHandler<ws::Message, ws::ProtocolError> for Ws {
+    fn handle(&mut self, msg: ws::Message, ctx: &mut Self::Context) {
+        debug!("WS: {:?}", msg);
+        match msg {
+            ws::Message::Ping(msg) => ctx.pong(&msg),
+            ws::Message::Text(text) => ctx.text(text),
+            ws::Message::Binary(bin) => ctx.binary(bin),
+            _ => (),
+        }
+    }
+}
+
+fn main() {
+    setup();
+
+    let data = GlobalData::new();
+
+    let app = move || {
+        let d = data.clone();
+        App::with_state(d)
+            .middleware(SetDefaultHeaders) // Sets 'server' header
+            .resource("/xaxa", |r| r.method(Method::GET).f(|req| {
+                info!("{:?}", req);
+                match *req.method() {
+                    Method::GET => HttpResponse::Ok(),
+                    _ => HttpResponse::NotFound(),
+                }
+            }))
+            .resource("/1", |r| r.method(Method::GET).f(|req| {
+                HttpResponse::Ok()
+                    .content_type("text/html; charset=utf-8")
+                    .header("server", "icc")
+                    .body(format!("1"))
+                // .finish()
+            }))
+
+            .resource("/s/", |r| {
+                r.f(|req| {
+                    ws::start(req, Ws)
+                })
+            })
+
+            .resource("/", |r| {
+                r.method(Method::GET).f(move |req| {
+                    let state : &Arc<RwLock<GlobalData>> = req.state();
+                    let is_down = state.read().unwrap().is_down;
+
+                    if is_down == true {
+                        state.write().unwrap().is_down = false;
+                    } else {
+                        state.write().unwrap().is_down = true;
+                    }
+
+
+                    HttpResponse::Ok()
+                        .content_type("text/html; charset=utf-8")
+                        .header("server", "icc")
+                        .body(format!("{}", is_down))
+                })
+            })
+    };
+
+    server::new(app)
+        .bind("0.0.0.0:4017")
+        .unwrap()
+        .shutdown_timeout(0)
+        .run();
+}
+
+#[cfg(debug_assertions)]
+fn setup() {
+    env::set_var("RUST_LOG", "trace,tokio_reactor=info,mio=info,actix_net=info,actix_web=info");
+    pretty_env_logger::init();
+}
+
+#[cfg(not(debug_assertions))]
+fn setup() {
+
+}
